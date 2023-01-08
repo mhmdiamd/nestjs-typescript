bff1575086bcbbdf0134d039f707facb65372d9b
mhmdiamd <darmawanilham34@gmail.com>
2023-01-08 20:39:45 +0700

    add Config database to postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,14 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm/dist';
+import { TypeOrmConfig } from './tasks/config/typeorm.config';
 import { TasksModule } from './tasks/tasks.module';
 
 @Module({
-  imports: [TasksModule],
+  imports: [
+    /*Configuration Database using postgress*/ TypeOrmModule.forRoot(
+      TypeOrmConfig,
+    ),
+    TasksModule,
+  ],
 })
 export class AppModule {}
--- src/tasks/config/typeorm.config.ts
+++ src/tasks/config/typeorm.config.ts
@@ -1,1 +1,12 @@
-export const typeormConfigModule = {};
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+export const TypeOrmConfig: TypeOrmModuleOptions = {
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'postgres',
+  password: 'ilham',
+  database: 'taskmanagement',
+  entities: [__dirname + '/../**/*.entity.ts'],
+  synchronize: true,
+};
